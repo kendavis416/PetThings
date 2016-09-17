69640c4a0cbb5f8772b6cd1f3a6477c906ee2c2d
root <root@430bcb70cf84>
2016-09-17 04:11:00 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,11 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "mobile",
+  "defaultLanguage" : "en",
+  "displayName" : "PetThings",
+  "homePage" : "Main",
+  "name" : "PetThings",
+  "platformType" : "MOBILE",
+  "supportedLanguages" : "en",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
